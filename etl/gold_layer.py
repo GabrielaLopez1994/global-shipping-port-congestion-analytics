51ff325c15e0415219b7e97aa961791008a83de3
GabrielaLopez1994 <79812269+GabrielaLopez1994@users.noreply.github.com>
2026-09-22 16:37:44 -0400

    Create gold_layer.py

---
+++ etl/gold_layer.py
@@ -0,0 +1,15 @@
+"""
+Gold Layer
+
+Global Shipping & Port Congestion Analytics
+"""
+
+# Read Silver
+
+# Filter analytical entities
+
+# Build Dimensions
+
+# Build Fact Table
+
+# Persist Gold Tables
