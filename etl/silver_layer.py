1e02297aa8e342f0ba4439754b0ec3d7b32c5e1b
GabrielaLopez1994 <79812269+GabrielaLopez1994@users.noreply.github.com>
2026-09-22 10:11:42 -0400

    Create silver_layer.py

---
+++ etl/silver_layer.py
@@ -0,0 +1,18 @@
+"""
+Silver Layer
+
+Global Shipping & Port Congestion Analytics
+
+"""
+
+# Read Bronze
+
+# Flatten country
+
+# Flatten indicator
+
+# Data quality rules
+
+# MD5 Hash
+
+# Persist to Silver
